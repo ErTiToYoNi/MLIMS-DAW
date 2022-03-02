bfaf8c805f38fe3b1c659accf5d9a176ab8a78c2
Yon <ybollainaof@gmail.com>
2022-03-02 09:48:35 +0100

    cmabios 803

--- Unit8/803/scripts/main.js
+++ Unit8/803/scripts/main.js
@@ -1,12 +1,15 @@
 const resultadoTexto = document.querySelector("#resultado");
 const botonMultiply = document.querySelector("#multiply");
 const botonDivide= document.querySelector("#divide");
+const botonSum= document.querySelector("#sum");
 
 botonMultiply.addEventListener('click',multiplicar);
 botonDivide.addEventListener('click',dividir);
+botonSum.addEventListener('click',suma);
 
 function multiplicar(){
-    let firstNumber = document.querySelector("#first-number").value;
+    
+    let firstNumber = inputPrimerNumero;
     let secondNumber = document.querySelector("#second-number").value;
     firstNumber = parseInt(firstNumber, 10);
     secondNumber = parseInt(secondNumber, 10);
@@ -19,4 +22,22 @@ function dividir(){
     firstNumber = parseInt(firstNumber, 10);
     secondNumber = parseInt(secondNumber, 10);
     resultadoTexto.textContent = firstNumber / secondNumber;
+}
+
+function suma(){
+    var firstNumber = inputPrimerNumero;
+    var secondNumber= inputSegundoNumero;
+    resultadoTexto.textContent = firstNumber + secondNumber;
+}
+
+function inputPrimerNumero(){
+    let firstNumber = document.querySelector("#first-number").value;
+    firstNumber = parseInt(firstNumber, 10);
+    return firstNumber;
+}
+
+function inputSegundoNumero(){
+    let secondNumber = document.querySelector("#second-number").value;
+    secondNumber = parseInt(firstNumber, 10);
+    return secondNumber;
 }
