0c2cfd02f2cb06f66a929d313e27e26dba18cc72
Yon <ybollainaof@gmail.com>
2022-03-15 09:40:48 +0100

    807 terminada

--- Unit8/806/script/main.js
+++ Unit8/806/script/main.js
@@ -3,13 +3,19 @@
 function main(){
     
     let divInfo = document.querySelector("#info");  
-    let paraphs = document.createElement('p');
+    let quest1 = document.createElement('p');
+    let quest2 = document.createElement('p');
     let article = document.querySelector("#article");
-    let paraphInDocument = article.getElementsByTagName("p")[0];
-    console.log(paraphInDocument);     
-    divInfo.append(paraphs);
-    paraphs.innerHTML= "The number of paragraphs on the page:";
+    let parrafos =document.querySelectorAll('p');
+    
+    quest1.innerHTML= `The number of paragraphs on the page: ${parrafos.length}`;
+    console.log();
+    divInfo.append(quest1);
 
+    quest2.innerHTML= `The text of the second paragraph: ${parrafos.length}`;
+    console.log();
+    divInfo.append(quest1);
+    
 }
 
 document.addEventListener("DOMContentLoaded", main);
---
+++ Unit8/807/scripts/main.js
@@ -0,0 +1,36 @@
+function main() {
+
+    let divLibros = document.querySelector("#libros");
+
+    let harryPotter = {
+        title: 'Harry Potter 1',
+        autor: 'JK Rowling',
+        publishedIn: 1997,
+        alredyRead: true,
+    };
+    let harryPotter2 = {
+        title: 'Harry Potter 2',
+        autor: 'JK Rowling',
+        publishedIn: 1998,
+        alredyRead: false,
+    };
+    let harryPotter3 = {
+        title: 'Harry Potter 3',
+        autor: 'JK Rowling',
+        publishedIn: 1999,
+        alredyRead: false,
+    };
+
+    let libros = [harryPotter, harryPotter2, harryPotter3];
+
+    for (let i = 0; i < libros.length; i++) {
+        let newParaph = document.createElement('p');
+            if(libros[i].alredyRead===true){
+                newParaph.innerHTML = "Ya leiste "+libros[i].title +" by "+ libros[i].autor;
+            }
+            else
+                newParaph.innerHTML = "Necesitas leer "+libros[i].title +" by "+ libros[i].autor;
+        divLibros.appendChild(newParaph);
+    }
+}
+document.addEventListener("DOMContentLoaded", main);
