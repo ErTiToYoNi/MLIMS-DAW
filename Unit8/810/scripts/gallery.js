b2e2faa7280aef10bd570afb71a80971b204c0e9
Yon <ybollainaof@gmail.com>
2022-03-29 09:44:13 +0200

    810

---
+++ Unit8/810/scripts/gallery.js
@@ -0,0 +1,54 @@
+
+function main(){
+    let buttonNext = document.querySelector("#buttonNext");
+    let buttonPrevious = document.querySelector("#buttonPrevious");
+    let titleBook = document.querySelector("#title");
+    let authorBook = document.querySelector("#author");
+    let imagen = document.querySelector("#cover");
+
+
+    imagen.src = "covers/harrypotter1.jpg"; 
+    titleBook.innerHTML = books[0].title;
+    authorBook.innerHTML = books[0].author;
+    let posicion = 0;
+
+    for(let key in books){ 
+        buttonNext.addEventListener('click',nextCover);
+        buttonPrevious.addEventListener('click',previousCover);
+    }
+
+    function nextCover(){ 
+            posicion++;
+            console.log(posicion);
+            if(posicion == (books.length-1)){
+                buttonNext.disabled= true;
+            }
+            else{
+                imagen.src = "covers/"+ books[posicion].cover;
+                imagen.alt = books[posicion].cover;
+                titleBook.innerHTML = books[posicion].title;
+                authorBook.innerHTML = books[posicion].author;
+            }
+            if(posicion!=0){
+                buttonPrevious.disabled= false;
+            }
+        }
+
+    function previousCover(){
+        posicion--;
+        console.log(posicion);
+        if(posicion == 0){
+            buttonPrevious.disabled = true;
+        }
+        else{
+        imagen.src = "covers/"+books[posicion].cover;
+        titleBook.innerHTML = books[posicion].title;
+        authorBook.innerHTML = books[posicion].author;
+        }
+        if(posicion== books.length){
+            buttonNext.disabled= false;
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", main);
---
+++ Unit8/810/scripts/main.js
@@ -0,0 +1,24 @@
+let harryPotter = {
+    title: 'Harry Potter 1',
+    author: 'JK Rowling',
+    publishedIn: 1997,
+    alreadyRead: true,
+    cover: 'harrypotter1.jpg',
+};
+let harryPotter2 = {
+    title: 'Harry Potter 2',
+    author: 'JK Rowling',
+    publishedIn: 1998,
+    alreadyRead: false,
+    cover: 'harrypotter2.jpg',
+};
+let harryPotter3 = {
+    title: 'Harry Potter 3',
+    author: 'JK Rowling',
+    publishedIn: 1999,
+    alreadyRead: false,
+    cover: 'harrypotter3.jpg',
+};
+
+
+let books = [harryPotter, harryPotter2, harryPotter3];
